2011f41e32dfcd21f4dcf494cb434bf253163e28
Steve Baskauf <steve.baskauf@vanderbilt.edu>
2019-11-09 23:57:52 -0500

    upload revised Wikibase CSV processing script

--- publications/wikidata/process_csv_metadata.py
+++ publications/wikidata/process_csv_metadata.py
@@ -1,4 +1,154 @@
+# See https://github.com/HeardLibrary/digital-scholarship/blob/master/code/wikibase/api/write-statements.py
+# for explanation of how to write to a Wikibase API and comments on the authentication functions
+
 import json
+import requests
+import csv
+from pathlib import Path
+from time import sleep
+
+# -----------------------------------------------------------------
+# function definitions
+
+def retrieveCredentials(path):
+    with open(path, 'rt') as fileObject:
+        lineList = fileObject.read().split('\n')
+    endpointUrl = lineList[0].split('=')[1]
+    username = lineList[1].split('=')[1]
+    password = lineList[2].split('=')[1]
+    credentials = [endpointUrl, username, password]
+    return credentials
+
+def getLoginToken(apiUrl):    
+    parameters = {
+        'action':'query',
+        'meta':'tokens',
+        'type':'login',
+        'format':'json'
+    }
+    r = session.get(url=apiUrl, params=parameters)
+    data = r.json()
+    return data['query']['tokens']['logintoken']
+
+def logIn(apiUrl, token, username, password):
+    parameters = {
+        'action':'login',
+        'lgname':username,
+        'lgpassword':password,
+        'lgtoken':token,
+        'format':'json'
+    }
+    r = session.post(apiUrl, data=parameters)
+    data = r.json()
+    return data
+
+def getCsrfToken(apiUrl):
+    parameters = {
+        "action": "query",
+        "meta": "tokens",
+        "format": "json"
+    }
+    r = session.get(url=apiUrl, params=parameters)
+    data = r.json()
+    return data["query"]["tokens"]["csrftoken"]
+
+# read a CSV into a list of dictionaries
+def readDict(filename):
+    fileObject = open(filename, 'r', newline='', encoding='utf-8')
+    dictObject = csv.DictReader(fileObject)
+    array = []
+    for row in dictObject:
+        array.append(row)
+    fileObject.close()
+    return array
+
+# write specific types of statements
+def writeEntityValueStatement(apiUrl, editToken, subjectQNumber, propertyPNumber, objectQNumber):
+    strippedQNumber = objectQNumber[1:len(objectQNumber)] # remove initial "Q" from object string
+    # note: the value is a string, not an actual data structure.  I think it will get URL encoded by requests before posting
+    value ='{"entity-type":"item","numeric-id":' + strippedQNumber+ '}'
+    data = writeStatement(apiUrl, editToken, subjectQNumber, propertyPNumber, value)
+    return data
+
+# pass in the local names including the initial letter as strings, e.g. ('Q3345', 'P6', 'Q1917')
+def writeStatement(apiUrl, editToken, subjectQNumber, propertyPNumber, value):
+    parameters = {
+        'action':'wbcreateclaim',
+        'format':'json',
+        'entity':subjectQNumber,
+        'snaktype':'value',
+        'bot':'1',  # not sure that this actually does anything
+        'token': editToken,
+        'property': propertyPNumber,
+        'value': value
+    }
+    r = session.post(apiUrl, data=parameters)
+    data = r.json()
+    return data
+
+def writeLabel(apiUrl, editToken, subjectQNumber, languageCode, value):
+    parameters = {
+        'action':'wbsetlabel',
+        'format':'json',
+        'id':subjectQNumber,
+        'bot':'1',  # not sure that this actually does anything
+        'token': editToken,
+        'language': languageCode,
+        'value': value
+    }
+    r = session.post(apiUrl, data=parameters)
+    data = r.json()
+    return data
+
+def writeAltLabel(apiUrl, editToken, subjectQNumber, languageCode, value):
+    parameters = {
+        'action':'wbsetaliases',
+        'format':'json',
+        'id':subjectQNumber,
+        'bot':'1',  # not sure that this actually does anything
+        'token': editToken,
+        'language': languageCode,
+        'add': value
+    }
+    r = session.post(apiUrl, data=parameters)
+    data = r.json()
+    return data
+
+# ----------------------------------------------------------------
+# authentication
+
+# This is the format of the wikibase_credentials.txt file. Username and password
+# are for a bot that you've created.  Save file in your home directory.
+'''
+endpointUrl=https://test.wikidata.org
+username=User@bot
+password=465jli90dslhgoiuhsaoi9s0sj5ki3lo
+'''
+
+# Instantiate session outside of any function so that it's globally accessible.
+session = requests.Session()
+
+# default API resource URL when a Wikibase/Wikidata instance is installed.
+resourceUrl = '/w/api.php'
+
+home = str(Path.home()) # gets path to home directory; supposed to work for Win and Mac
+credentialsFilename = 'wikibase_credentials.txt'
+credentialsPath = home + '/' + credentialsFilename
+credentials = retrieveCredentials(credentialsPath)
+endpointUrl = credentials[0] + resourceUrl
+user = credentials[1]
+pwd = credentials[2]
+
+loginToken = getLoginToken(endpointUrl)
+data = logIn(endpointUrl, loginToken, user, pwd)
+csrfToken = getCsrfToken(endpointUrl)
+
+# -------------------------------------------
+# Beginning of script to process the tables
+
+# If using real Wikidata, need to implement maxlag parameter https://www.mediawiki.org/wiki/Manual:Maxlag_parameter
+# For now, throttle using a fixed number of seconds to delay
+delay = 0.2
 
 with open('csv-metadata.json', 'rt', encoding='utf-8') as fileObject:
     text = fileObject.read()
@@ -6,7 +156,10 @@ metadata = json.loads(text)
 
 tables = metadata['tables']
 for table in tables:
-    print(table['url'])
+    tableFileName = table['url']
+    print('File name: ', tableFileName)
+    tableData = readDict(tableFileName)
+    
     # assume each table has an aboutUrl and each table is about an entity
     # extract the column name of the subject resource from the URI template
     temp = table['aboutUrl'].partition('{')[2]
@@ -24,28 +177,65 @@ for table in tables:
         if not('suppressOutput' in column):
             
             # find the columns (if any) that provide labels
+            # Note: if labels exist for a language, they will be overwritten
             if column['propertyUrl'] == 'rdfs:label':
                 labelColumnHeader = column['titles']
                 labelLanguage = column['lang']
                 print('Label column: ', labelColumnHeader, ', language: ', labelLanguage)
-                
-            # find columns that contain alternate labels
+                for row in tableData:
+                    obj = row[labelColumnHeader]
+                    if obj != '':                  
+                        print(row[subjectWikidataIdName], labelLanguage, obj)
+                        data = writeLabel(endpointUrl, csrfToken, row[subjectWikidataIdName], labelLanguage, obj)
+                        print('Write confirmation: ', data)
+                        print()
+                        sleep(delay)
+
+            # find columns that contain aliases
+            # GUI calls it "Also known as"; RDF as skos:altLabel
             elif column['propertyUrl'] == 'skos:altLabel':
                 altLabelColumnHeader = column['titles']
                 altLabelLanguage = column['lang']
                 print('Alternate label column: ', altLabelColumnHeader, ', language: ', altLabelLanguage)
-            
+                for row in tableData:
+                    obj = row[altLabelColumnHeader]
+                    if obj != '':                  
+                        print(row[subjectWikidataIdName], altLabelLanguage, obj)
+                        data = writeAltLabel(endpointUrl, csrfToken, row[subjectWikidataIdName], altLabelLanguage, obj)
+                        print('Write confirmation: ', data)
+                        print()
+                        sleep(delay)
+         
             # find columns that contain properties with entity values
             elif 'valueUrl' in column:
                 propColumnHeader = column['titles']
                 propertyId = column['propertyUrl'].partition('prop/direct/')[2]
                 print('Property column: ', propColumnHeader, ', Property ID: ', propertyId)
-            
+                for row in tableData:
+                    obj = row[propColumnHeader]
+                    if obj != '':                  
+                        print(row[subjectWikidataIdName], propertyId, obj)
+                        data = writeEntityValueStatement(endpointUrl, csrfToken, row[subjectWikidataIdName], propertyId, row[propColumnHeader])
+                        print('Write confirmation: ', data)
+                        print()
+                        sleep(delay)
+
             # remaining columns should have properties with literal values
             else:
                 propColumnHeader = column['titles']
                 propertyId = column['propertyUrl'].partition('prop/direct/')[2]
                 valueDatatype = column['datatype']
                 print('Property column: ', propColumnHeader, ', Property ID: ', propertyId, ' Value datatype: ', valueDatatype)
+                for row in tableData:
+                    obj = row[propColumnHeader]
+                    if obj != '':                  
+                        print(row[subjectWikidataIdName], propertyId, obj)
+                        data = writeStatement(endpointUrl, csrfToken, row[subjectWikidataIdName], propertyId, '"' + row[propColumnHeader] + '"')
+                        print('Write confirmation: ', data)
+                        print()
+                        sleep(delay)
+                        
+            print()
+            # need to include an option for adding descriptions using wbsetdescription
 
     print()
