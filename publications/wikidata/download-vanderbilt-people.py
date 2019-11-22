3e77d33fbe66bddae406a31c5ff01aface749b1d
Steve Baskauf <steve.baskauf@vanderbilt.edu>
2019-11-22 12:41:09 -0600

    wikidata SPARQL query to get data on people employed at Vanderbilt

---
+++ publications/wikidata/download-vanderbilt-people.py
@@ -0,0 +1,51 @@
+import requests   # best library to manage HTTP transactions
+import json
+import csv
+
+# function to write results to a file
+def writeCsv(fileName, array):
+    fileObject = open(fileName, 'w', newline='', encoding='utf-8')
+    writerObject = csv.writer(fileObject)
+    for row in array:
+        writerObject.writerow(row)
+    fileObject.close()
+
+endpointUrl = 'https://query.wikidata.org/sparql'
+query = '''select distinct  ?person ?orcid ?startDate ?endDate ?description where {
+  ?person p:P108 ?statement.
+  ?statement ps:P108  wd:Q29052.
+  optional{?statement pq:P580 ?startDate.}
+  optional{?statement pq:P582 ?endDate.}
+  optional{?person wdt:P496 ?orcid.}
+  optional{
+    ?person schema:description ?description.
+    FILTER(lang(?description)="en")
+          }
+  }'''
+
+# The endpoint defaults to returning XML, so the Accept: header is required
+r = requests.get(endpointUrl, params={'query' : query}, headers={'Accept' : 'application/json'})
+
+data = r.json()
+print(json.dumps(data,indent = 2))
+
+table = [['wikidataIri', 'description', 'startDate', 'endDate', 'orcid']]
+items = data['results']['bindings']
+for item in items:
+    wikidataIri = item['person']['value']
+    description = ''
+    if 'description' in item:
+        description = item['description']['value']
+    startDate = ''
+    if 'startDate' in item:
+        startDate = item['startDate']['value']
+    endDate = ''
+    if 'endDate' in item:
+        endDate = item['endDate']['value']
+    orcid = ''
+    if 'orcid' in item:
+        orcid = item['orcid']['value']
+    table.append([wikidataIri, description, startDate, endDate, orcid])
+    
+fileName = 'vanderbilt_wikidata.csv'
+writeCsv(fileName, table)
