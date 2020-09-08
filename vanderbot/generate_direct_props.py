a33c8dfe4031a7d6cdd9d23c6f9b06dadab218ad
Steve Baskauf <steve.baskauf@vanderbilt.edu>
2020-09-08 15:13:51 -0500

    update script to add triples entailed by Wikibase model

--- vanderbot/generate_direct_props.py
+++ vanderbot/generate_direct_props.py
@@ -12,7 +12,7 @@ from pathlib import Path
 # Configuration data
 # ---------------
 
-graph_name = 'http://nursing'
+graph_name = 'https://raw.githubusercontent.com/HeardLibrary/linked-data/54bd94c609e9c5af6c558cd926939ded67cba2ae/json_schema/bluffton_presidents.csv'
 accept_media_type = 'text/turtle'
 sparql_endpoint = "https://sparql.vanderbilt.edu/sparql"
 request_header_dictionary = {
@@ -47,7 +47,7 @@ def load_credential(filename, directory):
         exit()
     return(cred)
 
-def retrieve_direct_statements(sparql_endpoint):
+def retrieve_direct_statements(sparql_endpoint, graph_name):
     query = '''
 construct {?item ?directProp ?value.}
 from <''' + graph_name + '''>
@@ -60,6 +60,26 @@ where {
   bind(iri(concat("http://www.wikidata.org/prop/direct/", ?id)) as ?directProp)
   }
 '''
+    results = []
+    r = requests.get(sparql_endpoint, params={'query' : query}, headers=request_header_dictionary)
+    return r.text
+
+def retrieve_time_statements(sparql_endpoint, graph_name, subject_type):
+    # Happily, each subject type: "statement", "reference", and "qualifier" contains 9 characters.
+    # so the string extraction is the same for all.
+    query = '''
+prefix wikibase: <http://wikiba.se/ontology#>
+construct {?subject ?directProp ?timeValue.}
+from <''' + graph_name + '''>
+where {
+  ?subject ?valueProperty ?value.
+  ?value wikibase:timeValue ?timeValue.
+  filter(substr(str(?valueProperty),1,45)="http://www.wikidata.org/prop/''' + subject_type + '''/value/")
+  bind(substr(str(?valueProperty),46) as ?id)
+  bind(iri(concat("http://www.wikidata.org/prop/''' + subject_type + '''/", ?id)) as ?directProp)
+  }
+'''
+    results = []
     r = requests.get(sparql_endpoint, params={'query' : query}, headers=request_header_dictionary)
     return r.text
 
@@ -70,32 +90,51 @@ def perform_sparql_update(sparql_endpoint, pwd, update_command):
     print(str(r.status_code) + ' ' + r.url)
     print(r.text)
 
+def prep_and_update(sparql_endpoint, pwd, graph_name, graph_text):
+    # remove prefixes from response Turtle, which are not necessary since IRIs are unabbreviated
+    graph_text_list = graph_text.split('\n')
+    # print(graph_text_list)
+    graph_text = ''
+    for line in graph_text_list:
+        try:
+            if line[0] != '@':
+                graph_text += line + '\n'
+        except:
+            pass
+    #print()
+    #print(graph_text)
+
+    if len(graph_text) != 0: # don't perform an update if there aren't any triples to add
+        # Send SPARQL 1.1 UPDATE to endpoint to add the constructed triples into the graph
+        update_command = '''INSERT DATA
+        { GRAPH <''' + graph_name + '''> { 
+        ''' + graph_text + '''
+        }}'''
+
+        #print(update_command)
+        perform_sparql_update(sparql_endpoint, pwd, update_command)
+    else:
+        print('no triples to write')
+
 # ---------------
 # Construct the direct property statements entailed by the Wikibase model and retrieve from endpoint 
 # ---------------
-
-graph_text = retrieve_direct_statements(sparql_endpoint)
-print('constructed triples retrieved')
-
-# remove prefixes from response Turtle, which are not necessary since IRIs are unabbreviated
-graph_text_list = graph_text.split('\n')
-graph_text = ''
-for line in graph_text_list:
-    try:
-        if line[0] != '@':
-            graph_text += line + '\n'
-    except:
-        pass
-
-# Send SPARQL 1.1 UPDATE to endpoint to add the constructed triples into the graph
-
-update_command = '''INSERT DATA
-{ GRAPH <''' + graph_name + '''> { 
-''' + graph_text + '''
-}}'''
-
 pwd = load_credential(filename, directory)
-perform_sparql_update(sparql_endpoint, pwd, update_command)
+
+graph_text = retrieve_direct_statements(sparql_endpoint, graph_name)
+#print(graph_text)
+print('constructed direct triples retrieved')
+
+prep_and_update(sparql_endpoint, pwd, graph_name, graph_text)
+print()
+
+for subject_type in ['statement', 'reference', 'qualifier']:
+    graph_text = retrieve_time_statements(sparql_endpoint, graph_name, subject_type)
+    #print(graph_text)
+    print('constructed direct ' + subject_type + ' time triples retrieved')
+
+    prep_and_update(sparql_endpoint, pwd, graph_name, graph_text)
+    print()
 
 print()
 print('done')
