6988bd18d6aa828ff5c1c0fc4d0a6edac6e81018
Steve Baskauf <steve.baskauf@vanderbilt.edu>
2021-03-08 19:16:14 -0600

    clarify file paths

--- vanderbot/convert_json_to_metadata_schema.py
+++ vanderbot/convert_json_to_metadata_schema.py
@@ -16,7 +16,7 @@ import sys # Read CLI arguments
 if len(sys.argv) == 2: # if exactly one argument passed (i.e. the configuration file path)
     config_path = sys.argv[1] # sys.argv[0] is the script name
     out_file_path = 'csv-metadata.json'
-elif len(sys.argv) == 3: # if two arguments passed, 1st is config path, 2nd is output path
+elif len(sys.argv) == 3: # if two arguments passed, 1st is config file path, 2nd is output file path
     config_path = sys.argv[1]
     out_file_path = sys.argv[2]
 else:
